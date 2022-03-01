9df2a8638697aa0f7bd9986eb0966e5eb50aa7a7
Daniel Skantze <daniel@getcoupleness.com>
2022-03-01 11:38:37 +0100

    Fix app packaging problem, hide dev console per default

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as crypto from 'crypto';
 // plugin that tells the Electron app where to look for the Webpack-bundled app code (depending on
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -26,15 +27,16 @@ const createWindow = (): void => {
         width: 800,
         webPreferences: {
             nodeIntegration: false,
-            preload: path.join(app.getAppPath(), 'src/preload.js'),
+            preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
         },
     });
 
     // and load the index.html of the app.
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
+    console.log("STARTING UP");
+    console.log(MAIN_WINDOW_WEBPACK_ENTRY);
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 };
 
 function getConfigPath(file?: string): string {
